b17dd326b398bc7c11743acddc2c3f94419d3be7
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-05-12 11:13:32 +0300

    Add files via upload

---
+++ CurrentPreviousLag.sql
@@ -0,0 +1,9 @@
+SELECT 
+    ticker, 
+    date, 
+    close, 
+    ROUND(((close - LAG(close) OVER (PARTITION BY ticker ORDER BY date)) / LAG(close) OVER (PARTITION BY ticker ORDER BY date)) * 100, 2) AS percentage_change
+FROM 
+    stock_prices
+LIMIT 
+    20;
