830538c223b530c36842e0de522c740e6efb6cf6
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-07-03 10:54:19 +0300

    Update FullOuterJoin.sql

--- FullOuterJoin.sql
+++ FullOuterJoin.sql
@@ -2,3 +2,24 @@ SELECT  a.Item AS ItemCart1,
         b.Item AS ItemCart2
 FROM    #Cart1 a FULL OUTER JOIN
         #Cart2 b ON a.Item = b.Item;
+
+
+Write an SQL statement to transform the following tables into the expected output.
+  Item              Item
+  -----             -----
+  Sugar             Sugar
+  Bread             Bread
+  Juice             Butter
+  Soda              Cheese
+  Flour             Fruit
+
+ Item Cart 1       Item Cart 2
+ ------------      ------------
+  Sugar             Sugar
+  Bread             Bread
+  Juice            
+  Soda
+  Flour
+                    Butter
+                    Cheese
+                    Fruit
