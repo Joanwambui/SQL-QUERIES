d0c1784ca2da3d4dac2027dd77d690bea4208247
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-07-17 19:57:06 +0300

    Add files via upload

---
+++ SUMOVERORDERBY.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS #Inventory;
+GO
+
+CREATE TABLE #Inventory
+(
+InventoryDate       DATE PRIMARY KEY,
+QuantityAdjustment  INTEGER NOT NULL
+);
+GO
+
+INSERT INTO #Inventory (InventoryDate, QuantityAdjustment) VALUES
+('7/1/2018',100),('7/2/2018',75),('7/3/2018',-150),
+('7/4/2018',50),('7/5/2018',-75);
+GO
+
+SELECT *
+FROM #Inventory
+
+SELECT InventoryDate, QuantityAdjustment, SUM(QuantityAdjustment) OVER (ORDER BY InventoryDate) AS Inventory
+FROM #Inventory
