9705fca74754b303995c07104024d7aa46e2143c
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-07-23 22:41:46 +0300

    Add files via upload

---
+++ Gapstarts_Gapendsusingleadfunction.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS #SeatingChart;
+GO
+
+CREATE TABLE #SeatingChart
+(
+SeatNumber  INTEGER PRIMARY KEY
+);
+GO
+
+INSERT INTO #SeatingChart (SeatNumber) VALUES
+(7),(13),(14),(15),(27),(28),(29),(30),(31),(32),(33),(34),(35),(52),(53),(54);
+GO
+
+---Gap starts and gap ends
+
+SELECT *
+FROM #SeatingChart
+
+
+WITH cte_Gaps AS 
+(
+SELECT  SeatNumber AS GapStart,
+        LEAD(SeatNumber,1,0) OVER (ORDER BY SeatNumber) AS GapEnd,
+        LEAD(SeatNumber,1,0) OVER (ORDER BY SeatNumber) - SeatNumber AS Gap
+FROM    #SeatingChart
+)
+SELECT  GapStart + 1 AS GapStart,
+        GapEnd - 1 AS GapEnd
+FROM    cte_Gaps
+WHERE Gap > 1;
+GO
