7bf8cf346ece0483da59df36d0cfb31f05f9d5b5
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-07-25 01:17:00 +0300

    Add files via upload

---
+++ Calculating number of missing values in a list.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS #SeatingChart;
+GO
+
+CREATE TABLE #SeatingChart
+(
+SeatNumber  INTEGER PRIMARY KEY
+);
+GO
+
+INSERT INTO #SeatingChart (SeatNumber) VALUES
+(7),(13),(14),(15),(27),(28),(29),(30),(31),(32),(33),(34),(35),(52),(53),(54);
+GO
+
+--Place a value of 0 in the SeatingChart table
+INSERT INTO #SeatingChart (SeatNumber) VALUES (0);
+GO
+
+
+SELECT *
+FROM #SeatingChart
+
+WITH cte_Rank
+AS
+(
+SELECT  SeatNumber,
+        ROW_NUMBER() OVER (ORDER BY SeatNumber) AS RowNumber,
+        SeatNumber - ROW_NUMBER() OVER (ORDER BY SeatNumber) AS Rnk
+FROM    #SeatingChart
+WHERE   SeatNumber > 0
+)
+SELECT  MAX(Rnk) AS MissingNumbers 
+FROM    cte_Rank;
+GO
