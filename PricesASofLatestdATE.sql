3b66a038a2bc81e3021f90261bb7bd95d301450d
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-07-26 01:11:11 +0300

    Add files via upload

---
+++ PricesASofLatestdATE.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS #ValidPrices;
+GO
+
+CREATE TABLE #ValidPrices
+(
+ProductID      INTEGER,
+UnitPrice      MONEY,
+EffectiveDate  DATE,
+PRIMARY KEY (ProductID, UnitPrice, EffectiveDate)
+);
+GO
+
+INSERT INTO #ValidPrices (ProductID, UnitPrice, EffectiveDate) VALUES
+(1001,1.99,'1/01/2018'),
+(1001,2.99,'4/15/2018'),
+(1001,3.99,'6/8/2018'),
+(2002,1.99,'4/17/2018'),
+(2002,2.99,'5/19/2018');
+GO
+
+SELECT *
+FROM #ValidPrices
+
+SELECT ProductID, EffectiveDate, UnitPrice
+FROM #ValidPrices
+WHERE EffectiveDate = MAX(EffectiveDate)
+GROUP BY ProductID
+
+SELECT VP.ProductID, VP.EffectiveDate, VP.UnitPrice
+FROM #ValidPrices VP
+INNER JOIN (
+    SELECT ProductID, MAX(EffectiveDate) AS LatestEffectiveDate
+    FROM #ValidPrices
+    GROUP BY ProductID
+) AS LatestPrices
+ON VP.ProductID = LatestPrices.ProductID
+AND VP.EffectiveDate = LatestPrices.LatestEffectiveDate;
