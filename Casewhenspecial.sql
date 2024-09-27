6386222cbfe9907690da2e47116573dd5c01d5b0
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-09-27 21:57:15 +0300

    Add files via upload

---
+++ Casewhenspecial.sql
@@ -0,0 +1,18 @@
+WITH cte AS (
+    SELECT COUNT(*) AS YTotal
+    FROM customers
+    WHERE has_member_card = 'Y'
+),
+cte2 AS (
+    SELECT COUNT(*) AS Total
+    FROM customers
+)
+
+SELECT (YTotal * 100.0 / Total) AS PercentageWithCard
+FROM cte, cte2;
+
+
+SELECT 
+    ROUND(CAST(COUNT(CASE WHEN has_member_card = 'Y' THEN 1 END) AS DECIMAL) / COUNT(*) * 100, 2) AS Percentage_with_Membership
+FROM customers
+WHERE kroger_id IS NOT NULL;
