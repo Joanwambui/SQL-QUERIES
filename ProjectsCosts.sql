f6c3946f0874818da0c715c230982ad1c984525e
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-10-28 10:26:19 +0300

    Add files via upload

---
+++ ProjectsCosts.sql
@@ -0,0 +1,10 @@
+with cte as (
+    select lp.title, lp.budget, sum(le.salary) as total_cost, DATEDIFF(day, start_date, end_date) as duration, ceiling(sum(le.salary) * ((DATEDIFF(day, start_date, end_date) * 1.0) / 365)) as prorated_employee_expense
+    from linkedin_projects lp
+    join linkedin_emp_projects lep on lp.id = lep.project_id
+    join linkedin_employees le on le.id = lep.emp_id
+    group by lp.title, lp.budget, start_date, end_date
+)
+select title, budget, prorated_employee_expense
+from cte
+where prorated_employee_expense > budget
