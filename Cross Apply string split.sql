b694fb74228c43b186b37c4a3beb77fe86da6ef8
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-10-29 19:46:38 +0300

    Add files via upload

---
+++ Cross Apply string split.sql
@@ -0,0 +1,7 @@
+SELECT 
+    VALUE,
+    COUNT(*) as No_Of_Occurrences
+FROM google_file_store
+CROSS APPLY STRING_SPLIT(REPLACE(REPLACE(contents , ',', '') , '.' , '') , ' ')
+WHERE filename in ('draft1.txt' , 'draft2.txt')
+GROUP BY VALUE;
