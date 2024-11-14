4130cbd0296453f333f42eddc2a79b6882db1d16
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-11-14 13:27:00 +0300

    Add files via upload

---
+++ Descriptionlike%.sql
@@ -0,0 +1,19 @@
+--METHOD 1
+/*
+SELECT DISTINCT winery
+FROM winemag_p1
+WHERE description LIKE '%plum[,. /-]%'
+OR description LIKE '%cherry[,. /-]%' 
+OR description LIKE '%rose[,. /-]%' 
+OR description LIKE '%hazelnut[,. /-]%' 
+ORDER BY 1
+*/
+
+--METHOD 2
+SELECT DISTINCT winery
+FROM winemag_p1
+WHERE description LIKE '%[^a-z]plum[^a-z]%'
+OR description LIKE '%[^a-z]cherry[^a-z]%' 
+OR description LIKE '%[^a-z]rose[^a-z]%' 
+OR description LIKE '%[^a-z]hazelnut[^a-z]%' 
+ORDER BY 1
