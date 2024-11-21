3232136ca67b7ca9baee99f4952ed6b2aa078560
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-11-21 23:36:58 +0300

    Add files via upload

---
+++ UsngLeadtogetnextemployer.sql
@@ -0,0 +1,35 @@
+--Consider all LinkedIn users who, at some point, worked at Microsoft. 
+--For how many of them was Google their next employer right after Microsoft (no employers in between)?
+
+
+CREATE TABLE linkedin_users (user_id INT,employer VARCHAR(255),position VARCHAR(255),start_date DATETIME,end_date DATETIME);
+
+INSERT INTO linkedin_users (user_id, employer, position, start_date, end_date) VALUES(1, 'Microsoft', 'developer', '2020-04-13', '2021-11-01'),
+(1, 'Google', 'developer', '2021-11-01', NULL),(2, 'Google', 'manager', '2021-01-01', '2021-01-11'),(2, 'Microsoft', 'manager', '2021-01-11', NULL),
+(3, 'Microsoft', 'analyst', '2019-03-15', '2020-07-24'),(3, 'Amazon', 'analyst', '2020-08-01', '2020-11-01'),
+(3, 'Google', 'senior analyst', '2020-11-01', '2021-03-04'),(4, 'Google', 'junior developer', '2018-06-01', '2021-11-01'),
+(4, 'Google', 'senior developer', '2021-11-01', NULL),(5, 'Microsoft', 'manager', '2017-09-26', NULL),(6, 'Google', 'CEO', '2015-10-02', NULL);
+
+SELECT *
+FROM linkedin_users
+
+--I want you to write a script that will chack for all user_ids after they worked for google their immediate next hire was microsoft 
+--Return user_ids that fit this criteria
+
+WITH cte AS (
+    SELECT
+        user_id,
+        employer,
+        start_date,
+        end_date,
+        LEAD(employer) OVER (PARTITION BY user_id ORDER BY start_date) AS next_employer
+    FROM
+        linkedin_users
+)
+SELECT
+    user_id
+FROM
+    cte
+WHERE
+    employer = 'Google'
+    AND next_employer = 'Microsoft';
