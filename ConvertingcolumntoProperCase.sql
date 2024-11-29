c985a34cb9ccfc2c9af22db1ed3e75dde10609f6
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-11-30 00:56:46 +0300

    Add files via upload

---
+++ ConvertingcolumntoProperCase.sql
@@ -0,0 +1,10 @@
+SELECT 
+    content_id,
+    content_text AS Original_Text,
+    STRING_AGG(UPPER(LEFT(value, 1)) + LOWER(SUBSTRING(value, 2, LEN(value))), ' ') AS Modified_Text
+FROM 
+    user_content
+CROSS APPLY 
+    STRING_SPLIT(content_text, ' ') -- Split the text into words
+GROUP BY 
+    content_id, content_text;
