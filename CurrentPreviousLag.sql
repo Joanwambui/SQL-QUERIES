b3e5170c15d1e10a127bee3765a17402eda376d2
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2025-03-01 22:17:27 +0300

    Update CurrentPreviousLag.sql

--- CurrentPreviousLag.sql
+++ CurrentPreviousLag.sql
@@ -5,5 +5,6 @@ SELECT
     ROUND(((close - LAG(close) OVER (PARTITION BY ticker ORDER BY date)) / LAG(close) OVER (PARTITION BY ticker ORDER BY date)) * 100, 2) AS percentage_change
 FROM 
     stock_prices
+    
 LIMIT 
     20;
