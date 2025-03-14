8c6c617aa371dc4a84961a3e71ab64f83a30597c
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2025-03-14 23:30:54 +0300

    Update Commonvaluebetweentwotables.sql

--- Commonvaluebetweentwotables.sql
+++ Commonvaluebetweentwotables.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS users1
 CREATE TABLE users1(user_id INT, user_name varchar(30));
 INSERT INTO users1 VALUES (1, 'Karl'), (2, 'Hans'), (3, 'Emma'), (4, 'Emma'), (5, 'Mike'), (6, 'Lucas'), (7, 'Sarah'), (8, 'Lucas'), (9, 'Anna'), (10, 'John');
 
+
 DROP TABLE IF EXISTS friends1
 CREATE TABLE friends1(user_id INT, friend_id INT);
 INSERT INTO friends1 VALUES (1,3),(1,5),(2,3),(2,4),(3,1),(3,2),(3,6),(4,7),(5,8),(6,9),(7,10),(8,6),(9,10),(10,7),(10,9);
