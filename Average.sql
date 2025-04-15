a4a06c540f17554dcdc65fd0c17edc637643e63d
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2025-04-15 23:28:06 +0300

    Update Average.sql

--- Average.sql
+++ Average.sql
@@ -2,3 +2,9 @@
 SELECT city, property_type,AVG(cast(bathrooms as float)),AVG(cast(bedrooms as float))
 from airbnb_search_details
 GROUP BY city, property_type
+
+SELECT AVG(booking_cost) AS avg_booking_cost
+FROM fct_corporate_bookings f
+INNER JOIN dim_companies d
+  ON f.company_id = d.company_id
+WHERE strftime('%Y-%m', f.booking_date) = '2024-01';
