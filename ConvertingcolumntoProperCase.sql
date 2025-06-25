af4b903f8b5fa62dc119f8fcdc293294c5a299c2
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2025-06-25 23:03:58 +0300

    Update ConvertingcolumntoProperCase.sql

--- ConvertingcolumntoProperCase.sql
+++ ConvertingcolumntoProperCase.sql
@@ -2,6 +2,7 @@ SELECT
     content_id,
     content_text AS Original_Text,
     STRING_AGG(UPPER(LEFT(value, 1)) + LOWER(SUBSTRING(value, 2, LEN(value))), ' ') AS Modified_Text
+    
 FROM 
     user_content
 CROSS APPLY 
